9a0fb6fcf3b4f59b90c639c5fcb3908ee1a0dd2d
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2025-01-20 18:56:48 +0100

    Refactor InitiativeList to dynamically generate characters and update InitiativeElement to display initiative and name

--- src/InitiativeList/InitiativeElement.tsx
+++ src/InitiativeList/InitiativeElement.tsx
@@ -1,10 +1,15 @@
 import { Accordion, AccordionPanel, Box } from 'grommet';
 
-export const InitiativeElement = () => {
+export const InitiativeElement = (props: {
+	name: string;
+	initiative: number;
+}) => {
 	return (
 		<Accordion>
-			<AccordionPanel label="Panel 1">
-				<Box pad="medium" title="One"></Box>
+			<AccordionPanel label={`[${props.initiative}] ${props.name}`}>
+				<Box pad="medium" title="One">
+					[{props.initiative}] {props.name}
+				</Box>
 			</AccordionPanel>
 		</Accordion>
 	);
--- src/InitiativeList/InitiativeList.tsx
+++ src/InitiativeList/InitiativeList.tsx
@@ -2,17 +2,37 @@ import { Box, Grid } from 'grommet';
 import { InitiativeElement } from './InitiativeElement';
 
 export const InitiativeList = () => {
+	const characters = new Array(10)
+		.fill(0)
+		.map((_, index) => ({
+			name: `Character ${index + 1}`,
+			initiative: Math.floor(Math.random() * 20) + 1,
+		}))
+		.sort((a, b) => b.initiative - a.initiative);
+
+	const getBackgroundColor = (index: number) => {
+		switch (index) {
+			case 0:
+				return 'brand';
+			case 1:
+				return 'light-4';
+			case 2:
+				return 'light-3';
+			default:
+				return 'light-2';
+		}
+	};
+
 	return (
 		<Grid rows={['xsmall', '...']} gap="none">
-			<Box background="brand">
-				<InitiativeElement />
-			</Box>
-			<Box background="light-5">
-				<InitiativeElement />
-			</Box>
-			<Box background="light-2">
-				<InitiativeElement />
-			</Box>
+			{characters.map((character, index) => (
+				<Box key={character.name} background={getBackgroundColor(index)}>
+					<InitiativeElement
+						name={character.name}
+						initiative={character.initiative}
+					/>
+				</Box>
+			))}
 		</Grid>
 	);
 };
