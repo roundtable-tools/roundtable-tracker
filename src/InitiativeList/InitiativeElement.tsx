bdb9ba2f4e7d3e72b346fe0eba1391f824b791cb
ostatni5 <26521377+ostatni5@users.noreply.github.com>
2025-01-25 19:56:52 +0100

    Enhance InitiativeElement with dynamic state display and typing effect animation

--- src/InitiativeList/InitiativeElement.tsx
+++ src/InitiativeList/InitiativeElement.tsx
@@ -1,5 +1,7 @@
 import { Accordion, AccordionPanel, Box } from 'grommet';
 import { Character } from '../store/data';
+import { useRef } from 'react';
+import { motion, useInView } from 'motion/react';
 
 export const InitiativeElement = (props: {
 	character: Character;
@@ -8,7 +10,12 @@ export const InitiativeElement = (props: {
 	return (
 		<Accordion activeIndex={props.open ? 0 : []}>
 			<AccordionPanel
-				label={`[${props.character.initiative}] ${props.character.name} - ${props.character.state}`}
+				label={
+					<Box pad="medium" direction="row" gap="small">
+						{`[${props.character.initiative}] ${props.character.name}`}{' '}
+						<State state={props.character.state} />
+					</Box>
+				}
 			>
 				<Box pad="medium" title="One">
 					[{props.character.initiative}] {props.character.name}
@@ -17,3 +24,31 @@ export const InitiativeElement = (props: {
 		</Accordion>
 	);
 };
+
+const State = (props: { state: Character['state'] }) => {
+	return (
+		<Box as={'span'}>
+			<TypingEffect text={props.state} />
+		</Box>
+	);
+};
+
+export function TypingEffect({ text = 'Typing Effect' }: { text: string }) {
+	const ref = useRef(null);
+	const isInView = useInView(ref, { once: false });
+
+	return (
+		<div ref={ref} style={{ position: 'relative' }}>
+			{text.split('').map((letter, index) => (
+				<motion.span
+					key={`${text}-${index}`}
+					initial={{ opacity: 0 }}
+					animate={isInView ? { opacity: 1 } : {}}
+					transition={{ duration: 0.1, delay: index * 0.05 }}
+				>
+					{letter}
+				</motion.span>
+			))}
+		</div>
+	);
+}
