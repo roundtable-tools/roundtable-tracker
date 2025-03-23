0fcadf035257439885f8fc0942d54818c2d3b01c
Derstilon <quban123@gmail.com>
2025-03-23 21:11:14 +0000

    Add health and temporary health adjustment functionality to InitiativeElement

--- src/components/InitiativeList/InitiativeElement.tsx
+++ src/components/InitiativeList/InitiativeElement.tsx
@@ -1,11 +1,12 @@
-import { Accordion, AccordionPanel, Box, Button } from 'grommet';
+import { Accordion, AccordionPanel, Box, Button, Grid, MaskedInput, Text } from 'grommet';
 import { Character } from '@/store/data';
 import { TypingEffect } from '@/components/TypingEffect';
 import { useEncounterStore } from '@/store/instance';
 import { Close } from 'grommet-icons';
 import { RemoveCharacterCommand } from '@/CommandHistory/Commands/RemoveCharacterCommand';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CommandHistoryContext } from '@/CommandHistory/CommandHistoryContext';
+import { UpdateCharacterDataCommand } from '@/CommandHistory/Commands/UpdateCharacterDataCommand';
 
 export const InitiativeElement = (props: {
 	character: Character;
@@ -19,6 +20,31 @@ export const InitiativeElement = (props: {
 
 	const slayCharacter = () => {
 		executeCommand(new RemoveCharacterCommand({ uuid: props.character.uuid }));
+	};
+
+	const [healthChange, setHealthChange] = useState<number | undefined>(undefined);
+	const [tempHealthChange, setTempHealthChange] = useState<number | undefined>(undefined);
+	const handleTempHealthChange = () => {
+		const newTempHealth = props.character.tempHealth ?? 0 + (tempHealthChange ?? 0);
+		console.log('New temp health:', newTempHealth);
+		executeCommand(new UpdateCharacterDataCommand({
+			uuid: props.character.uuid,
+			tempHealth: newTempHealth,
+		}));
+		setTempHealthChange(undefined);
+	};
+
+	const handleHealthChange = () => {
+		const newTempHealth = (healthChange && healthChange < 0) ? (props.character.tempHealth ?? 0) - (healthChange ?? 0) : (props.character.tempHealth ?? 0);
+		const newHealth = (healthChange && healthChange >= 0) ? (props.character.health ?? 0) + (healthChange ?? 0) : (newTempHealth >= 0) ? (props.character.health ?? 0) : (props.character.health ?? 0) + (healthChange ?? 0);
+		console.log('New health:', newHealth);
+		console.log('New temp health:', newTempHealth);
+		executeCommand(new UpdateCharacterDataCommand({
+			uuid: props.character.uuid,
+			health: newHealth,
+			tempHealth: newTempHealth,
+		}));
+		setHealthChange(undefined);
 	};
 
 	return (
@@ -46,13 +72,78 @@ export const InitiativeElement = (props: {
 					</Box>
 				}
 			>
-				<Button
-					primary
-					icon={<Close />}
-					label="Slay"
-					color={'status-critical'}
-					onClick={slayCharacter}
-				/>
+				<Grid
+					onMouseUp={(e) => e.stopPropagation()}
+					columns={['1fr', '1fr']}
+					gap="small"
+					pad={{ horizontal: 'medium', vertical: 'small' }}
+					justifyContent="between"
+					alignContent="center"
+				>
+					<Box>
+						<Text size='xxlarge' gridArea="value" alignSelf='center'>{(props.character.health ?? 0) + (props.character.tempHealth ?? 0)}/{props.character.maxHealth ?? 0}</Text>
+						<Grid columns={['3fr', '1fr']} gap="small">
+							<MaskedInput
+								mask={[
+									{
+										length: [1, 6],
+										regexp: /^[+-]?\d*$/,
+										placeholder: '+gain/-loss',
+									},
+								]}
+								value={`${healthChange ?? ''}`}
+								onChange={(e) => {
+									const value = e.target.value;
+									if (value === '') {
+										setHealthChange(undefined);
+									} else {
+										setHealthChange(parseInt(value));
+									}
+								}}
+							/>
+							<Button
+								label="Set"
+								onClick={handleHealthChange}
+								primary
+								color={'status-ok'}
+								style={{ width: '100%' }}
+							/>
+							<MaskedInput
+								mask={[
+									{
+										length: [1, 5],
+										regexp: /^\d*$/,
+										placeholder: 'temp HP',
+									},
+								]}
+								value={`${tempHealthChange ?? ''}`}
+								onChange={(e) => {
+									const value = e.target.value;
+									if (value === '') {
+										setTempHealthChange(undefined);
+									} else {
+										setTempHealthChange(parseInt(value));
+									}
+								}}
+							/>
+							<Button
+								label="Set"
+								onClick={handleTempHealthChange}
+								primary
+								color={'status-ok'}
+								style={{ width: '100%' }}
+							/>
+						</Grid>
+					</Box>
+					<Button
+						style={{ width: "", borderRadius: '8px'}}
+						primary
+						icon={<Close />}
+						label="Slay"
+						color={'status-critical'}
+						onClick={slayCharacter}
+					/>
+				</Grid>
 			</AccordionPanel>
 		</Accordion>
 	);
