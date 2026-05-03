90192250011f4d4fe10fc0e1ab35698953626c45
Jakub Niechaj <quban123@gmail.com>
2026-05-03 12:34:11 +0200

    Fix routing (#66)
    * Fix routing
    
    * prettier
    
    * Fix routing 2

--- web-roundtable-tracker/src/main.tsx
+++ web-roundtable-tracker/src/main.tsx
@@ -1,15 +1,21 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import {
+	RouterProvider,
+	createHashHistory,
+	createRouter,
+} from '@tanstack/react-router';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
 import { CommandHistoryProvider } from './CommandHistory/CommandHistoryProvider';
 
+const hashHistory = createHashHistory();
+
 // Create a new router instance
 const router = createRouter({
 	routeTree,
-	basepath: '/roundtable-tracker',
+	history: hashHistory,
 });
 
 // Register the router instance for type safety
